1f3f769b02d63061ca6815df89ad9afaee912564
Bhargavi <bjshahasane@gmail.com>
2022-07-18 18:06:49 +0530

    Slider and announcement

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
             <Search style={{color:"gray",fontSize:16}}/>
           </SearchContainer>
         </Left>
-        <Center><Logo>LAMA</Logo></Center>
+        <Center><Logo>TREND </Logo></Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
           <MenuItem>SIGN IN</MenuItem>
